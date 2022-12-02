45f04187082c5c10d7ccdf61ca4dbed5f1398730
Guillaume Gosselin <guillaume@bauhem.com>
2022-12-02 18:00:40 -0500

    up

--- config/script-utils.js
+++ config/script-utils.js
@@ -2,12 +2,6 @@
 export default () => {
   return [
     {
-      src: 'https://d3e54v103j8qbb.cloudfront.net/js/jquery-3.5.1.min.dc5e7f18c8.js?site=6388d1f4ab22f1a8fc67233a',
-      type: "text/partytown",
-      integrity: 'sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=',
-      crossorigin: 'anonymous'
-    },
-    {
       src: '/js/webflow.js',
       type: "text/partytown",
       body: 'true'
